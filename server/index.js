35cbabeae84861f9cf3c32c5c3cd3e12fa0111e4
PhatBen234 <158719289+PhatBen234@users.noreply.github.com>
2025-03-07 12:57:00 +0700

    fix code

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import routes from "./source/routes/index.js";
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
